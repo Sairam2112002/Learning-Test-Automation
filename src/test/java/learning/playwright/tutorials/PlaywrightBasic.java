6d134c0a9fcca99a9f6828057b182ab374a36d24
Sairam Pittala <sairam2112002@gmail.com>
2023-10-17 19:29:02 +0530

    A project of Filling Form has been created using Playwright Java in Page Object Model(POM)

--- src/test/java/learning/playwright/tutorials/PlaywrightBasic.java
+++ src/test/java/learning/playwright/tutorials/PlaywrightBasic.java
@@ -11,6 +11,10 @@ public class PlaywrightBasic {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             page.navigate("https://www.youtube.com/");
+            System.out.println(page.title());
+            Locator searchBar = page.locator("css=input[name='search_query']");
+            searchBar.fill("Breaking Bad");
+            System.out.println(searchBar.inputValue());
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Screenshots\\example.png")));
         }
     }
