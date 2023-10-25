84ca1f64cce57ba53296f8aa81e2c9091d1ae4fa
Sairam2112002 <sairam2112002@gmail.com>
2023-10-25 18:31:06 +0530

    Sample commit from EPAM Laptop

--- src/test/java/learning/cucumberPlaywright/opencart/runners/OpencartRunner.java
+++ src/test/java/learning/cucumberPlaywright/opencart/runners/OpencartRunner.java
@@ -3,6 +3,6 @@ package learning.cucumberPlaywright.opencart.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, glue = {"learning.cucumberPlaywright.opencart.stepdefs"}, features = {"src/test/resources/features/cucumber-playwright/opencart/opencart.feature"})
+@CucumberOptions(plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, glue = {"learning"}, features = {"src/test/resources/features/cucumber-playwright/opencart/opencart.feature"})
 public class OpencartRunner extends AbstractTestNGCucumberTests {
 }
