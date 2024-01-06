c9724aaaa63be22d62aba039ba6035587e2c71d9
Sairam2112002 <sairam2112002@gmail.com>
2024-01-06 17:17:27 +0530

    Selenium - Drag and Drop completed

--- src/test/java/learning/selenium/tutorials/SeleniumBase.java
+++ src/test/java/learning/selenium/tutorials/SeleniumBase.java
@@ -4,19 +4,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class SeleniumBase {
-    protected static final SoftAssertions softAssertions = new SoftAssertions();
+    protected static SoftAssertions softAssertions;
     protected static WebDriver driver;
     protected static Select select;
+    protected static Actions actions;
 
     @BeforeTest
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        softAssertions = new SoftAssertions();
         driver.manage().window().maximize();
     }
 
