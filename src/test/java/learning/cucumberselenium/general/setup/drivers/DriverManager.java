44be06e3f2c61f24b1b142215fc962b81403a636
Sairam2112002 <sairam2112002@gmail.com>
2024-01-08 17:39:40 +0530

    Update DriverManager.java

--- src/test/java/learning/cucumberselenium/general/setup/drivers/DriverManager.java
+++ src/test/java/learning/cucumberselenium/general/setup/drivers/DriverManager.java
@@ -4,12 +4,16 @@ import learning.cucumberselenium.general.setup.Constants;
 import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
-    public static WebDriver getDriver() {
-        return switch(Constants.getBrowserName()) {
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    public static void setDriver() {
+        WebDriver driver =  switch(Constants.getBrowserName()) {
             case "chrome" -> new ChromeBrowserDriver().createDriver();
             case "edge" -> new EdgeBrowserDriver().createDriver();
             case "firefox" -> new FirefoxBrowserDriver().createDriver();
             default -> throw new IllegalArgumentException("Provided invalid Browser Type");
         };
+
+        tlDriver.set(driver);
     }
 }
