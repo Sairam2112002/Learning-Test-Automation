d29f5f5b1a7b8cda19bea80b0d36d2b18d7b5969
Sairam2112002 <sairam2112002@gmail.com>
2024-01-09 13:32:38 +0530

    Update HooksNOPCommerceSelenium.java

--- src/test/java/learning/cucumberselenium/nopcommerce/stepdefinitions/HooksNOPCommerceSelenium.java
+++ src/test/java/learning/cucumberselenium/nopcommerce/stepdefinitions/HooksNOPCommerceSelenium.java
@@ -3,50 +3,16 @@ package learning.cucumberselenium.nopcommerce.stepdefinitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import learning.cucumberselenium.general.setup.Constants;
-import learning.cucumberselenium.general.setup.drivers.DriverManager;
-import learning.cucumberselenium.general.utilities.PropertiesReader;
-import lombok.Getter;
-import lombok.Setter;
-import org.assertj.core.api.SoftAssertions;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import learning.cucumberselenium.general.setup.HooksMethods;
 
-public class HooksNOPCommerceSelenium {
-    public static SoftAssertions softAssertions;
-    public static WebDriver driver;
-
-    @Getter @Setter
-    private static Scenario currentScenario;
-
+public class HooksNOPCommerceSelenium extends HooksMethods {
     @Before
     public void before(Scenario scenario) {
-        softAssertions = new SoftAssertions();
-
-        DriverManager.setDriver();
-        driver = DriverManager.tlDriver.get();
-
-        setCurrentScenario(scenario);
-        PropertiesReader.readProperties();
+        beforeMethod(scenario);
     }
 
     @After
     public void after(Scenario scenario) {
-        if(Constants.isCaptureScreenshotsForFailedScenarios()) {
-            if(scenario.isFailed()) {
-                takeScreenshot(scenario);
-            }
-        } else if(Constants.isCaptureScreenshots()) {
-            takeScreenshot(scenario);
-        }
-
-        driver.quit();
-        softAssertions.assertAll();
-    }
-
-    private void takeScreenshot(Scenario scenario) {
-        final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        scenario.attach(screenshot, "image/png", scenario.getName());
+        afterMethod(scenario);
     }
 }
