11c8bcc5b1488d765149817f5088ea9b58e04413
Sairam2112002 <sairam2112002@gmail.com>
2024-01-10 17:06:00 +0530

    Update WebActionImplementations.java

--- src/test/java/learning/cucumberselenium/general/utilities/WebActionImplementations.java
+++ src/test/java/learning/cucumberselenium/general/utilities/WebActionImplementations.java
@@ -1,14 +1,34 @@
 package learning.cucumberselenium.general.utilities;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
 
 public class WebActionImplementations {
+    private Actions actions;
+
     public void openUrl(WebDriver driver, String url) {
         driver.get(url);
     }
 
     public String getText(WebElement element) {
         return element.getText();
+    }
+
+    public void scrollToElement(WebDriver driver, WebElement element) {
+        actions = new Actions(driver);
+        actions.moveToElement(element).build().perform();
+    }
+
+    public void openLinkInNewTab(WebDriver driver, WebElement element) {
+        actions = new Actions(driver);
+        actions.keyDown(Keys.CONTROL).click(element).keyUp(Keys.CONTROL).build().perform();
+    }
+
+    public void switchToNewlyOpenedTab(WebDriver driver, List<String> windowHandles) {
+        driver.switchTo().window(windowHandles.get(1));
     }
 }
