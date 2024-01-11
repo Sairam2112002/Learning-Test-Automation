3d2f7e63c6d7e975fe1a482286f6aa6667688035
Sairam2112002 <sairam2112002@gmail.com>
2024-01-11 12:13:56 +0530

    Update WebActionImplementations.java

--- src/test/java/learning/cucumberselenium/general/utilities/WebActionImplementations.java
+++ src/test/java/learning/cucumberselenium/general/utilities/WebActionImplementations.java
@@ -4,16 +4,22 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WebActionImplementations {
     private Actions actions;
     private List<String> windowHandles;
+    private WebDriverWait wait;
 
     public void openUrl(WebDriver driver, String url) {
         driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.urlToBe(url));
     }
 
     public String getText(WebElement element) {
@@ -33,5 +39,15 @@ public class WebActionImplementations {
     public void switchToNewlyOpenedTab(WebDriver driver) {
         windowHandles = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(windowHandles.get(1));
+    }
+
+    public void hoverOnElement(WebDriver driver, WebElement element) {
+        actions = new Actions(driver);
+        actions.moveToElement(element).build().perform();
+    }
+
+    public void waitForElementToBeVisible(WebDriver driver, WebElement element, long timeOutInSeconds) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSeconds));
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
