d472e4e0197997b97029a75f1279af0fc669b0cc
Sairam2112002 <sairam2112002@gmail.com>
2025-01-05 19:57:23 +0530

    APIKeys.java created for storing api keys as constants

---
+++ src/test/java/learning/utilities/constants/APIKeys.java
@@ -0,0 +1,16 @@
+package learning.utilities.constants;
+
+import java.util.HashMap;
+
+/**
+ * Available API Keys: <br><br>
+ * <ol>
+ *     <li>GitHub Fine Grain Token</li>
+ *     <li>New York Times API Key</li>
+ *     <li>Go Rest API Token</li>
+ *     <li>Open Weather Map API Key</li>
+ * </ol>
+ */
+public class APIKeys {
+    public static final HashMap<String, String> API_KEYS = new HashMap<>();
+}
