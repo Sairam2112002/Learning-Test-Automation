e57f13787baa167de2c715e6cdd04f37a3a305ba
Sairam2112002 <sairam2112002@gmail.com>
2025-01-07 11:28:35 +0530

    Browser binaries and drivers paths added

---
+++ src/test/java/learning/utilities/constants/BrowserBinariesAndDriversPaths.java
@@ -0,0 +1,8 @@
+package learning.utilities.constants;
+
+public class BrowserBinariesAndDriversPaths {
+    public static final String CHROME_BINARY_PATH = "src/test/resources/BrowserBinariesAndDrivers/Binaries/chrome-win64/chrome.exe";
+    public static final String CHROME_DRIVER_PATH = "src/test/resources/BrowserBinariesAndDrivers/Drivers/chromedriver.exe";
+    public static final String FIREFOX_DRIVER_PATH = "src/test/resources/BrowserBinariesAndDrivers/Drivers/geckodriver.exe";
+    public static final String EDGE_DRIVER_PATH = "src/test/resources/BrowserBinariesAndDrivers/Drivers/msedgedriver.exe";
+}
