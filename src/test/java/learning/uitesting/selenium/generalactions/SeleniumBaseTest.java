5b893c79ef8ed1691de6f83387736b09b0fc35a6
Sairam_Pittala <sairam_pittala@epam.com>
2025-02-25 14:18:22 +0530

    BeforeTest and AfterTest methods have been added

--- src/test/java/learning/uitesting/selenium/generalactions/SeleniumBaseTest.java
+++ src/test/java/learning/uitesting/selenium/generalactions/SeleniumBaseTest.java
@@ -5,16 +5,31 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 public class SeleniumBaseTest {
     protected WebDriver driver;
     protected Logger logger;
 
+    protected static final String NOP_COMMERCE_URL = "https://www.nopcommerce.com/en/demo";
+    protected static final String GOOGLE_SEARCH_URL = "https://www.google.com/";
+
     @BeforeClass
     public void baseSetup() {
-        driver = new ChromeDriver();
         logger = LogManager.getLogger(this.getClass());
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.close();
     }
 
     @AfterClass
