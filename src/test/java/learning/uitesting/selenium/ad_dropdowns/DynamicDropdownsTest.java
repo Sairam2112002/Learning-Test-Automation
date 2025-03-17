c343e156be66bea9bc20f76851c9091604f8fd0f
Sairam2112002 <sairam2112002@gmail.com>
2025-03-17 15:50:52 +0530

    Handling Dynamic dropdowns added

---
+++ src/test/java/learning/uitesting/selenium/ad_dropdowns/DynamicDropdownsTest.java
@@ -0,0 +1,36 @@
+package learning.uitesting.selenium.ad_dropdowns;
+
+import learning.uitesting.selenium.utilities.SeleniumBaseTest;
+import learning.uitesting.selenium.utilities.UtilityMethods;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class DynamicDropdownsTest extends SeleniumBaseTest {
+    private final UtilityMethods utilityMethods = new UtilityMethods();
+
+    public DynamicDropdownsTest() {
+        xpathLocators.put("DEPARTURE_CITY_DROPDOWN", "//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']");
+        xpathLocators.put("DEPARTURE_CITY_HYDERABAD_OPTION", "//a[@value='HYD']");
+        xpathLocators.put("ARRIVAL_CITY_VIJAYAWADA_OPTION", "(//a[@value='VGA'])[2]");
+        xpathLocators.put("DEPART_DATE_MAY_10", "(//table[@class='ui-datepicker-calendar'])[1]//tbody//tr[2]//td[6]");
+    }
+
+    @Test(priority = 1)
+    public void setUp() {
+        driver.get(RAHUL_SHETTY_ACADEMY_DROPDOWN_PRACTICE);
+    }
+
+    @Test(priority = 2)
+    public void dynamicDropdowns() {
+        driver.findElement(By.xpath(xpathLocators.get("DEPARTURE_CITY_DROPDOWN"))).click();
+
+        utilityMethods.waitForElementToBeVisible(xpathLocators.get("DEPARTURE_CITY_HYDERABAD_OPTION"));
+        driver.findElement(By.xpath(xpathLocators.get("DEPARTURE_CITY_HYDERABAD_OPTION"))).click();
+
+        utilityMethods.waitForElementToBeVisible(xpathLocators.get("ARRIVAL_CITY_VIJAYAWADA_OPTION"));
+        driver.findElement(By.xpath(xpathLocators.get("ARRIVAL_CITY_VIJAYAWADA_OPTION"))).click();
+
+        utilityMethods.waitForElementToBeVisible(xpathLocators.get("DEPART_DATE_MAY_10"));
+        driver.findElement(By.xpath(xpathLocators.get("DEPART_DATE_MAY_10"))).click();
+    }
+}
