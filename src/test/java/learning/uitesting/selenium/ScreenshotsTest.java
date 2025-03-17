bc333bc28735b9404eb9a4944cd3039b011bd5bf
Sairam2112002 <sairam2112002@gmail.com>
2025-03-17 09:30:58 +0530

    Taking screenshot of an element added

--- src/test/java/learning/uitesting/selenium/ScreenshotsTest.java
+++ src/test/java/learning/uitesting/selenium/ScreenshotsTest.java
@@ -1,8 +1,10 @@
 package learning.uitesting.selenium;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -10,7 +12,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class ScreenshotsTest extends SeleniumBaseTest {
-    @Test
+    @Test(priority = 1)
     public void takeScreenshot() {
         driver.get(LAMBDATEST_SELENIUM_PLAYGROUND);
 
@@ -19,6 +21,22 @@ public class ScreenshotsTest extends SeleniumBaseTest {
 
         try {
             FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "/target/screenshot.png"));
+        } catch (IOException exception) {
+            logger.error(Arrays.toString(exception.getStackTrace()));
+        }
+    }
+
+    @Test(priority = 2)
+    public void takeScreenshotOfSpecificElement() {
+        driver.get(LAMBDATEST_SELENIUM_PLAYGROUND);
+
+        WebElement lambdatestLogoImage = driver.findElement(By.xpath("//header[@id='header']/nav/div/div/div/div/div/a/img"));
+
+        TakesScreenshot screenshot = (TakesScreenshot) lambdatestLogoImage;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "/target/elementScreenshot.png"));
         } catch (IOException exception) {
             logger.error(Arrays.toString(exception.getStackTrace()));
         }
