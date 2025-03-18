8ba88fddb4aeb1af0b399bc4848bd124914e64dd
Sairam2112002 <sairam2112002@gmail.com>
2025-03-18 14:03:50 +0530

    GreenKart practice problem completed

--- src/test/java/learning/uitesting/selenium/exampleproblems/greenkartvegetables/GreenCartTest.java
+++ src/test/java/learning/uitesting/selenium/exampleproblems/greenkartvegetables/GreenCartTest.java
@@ -2,12 +2,13 @@ package learning.uitesting.selenium.exampleproblems.greenkartvegetables;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class GreenCartTest extends BaseTest {
-    private static int expectedPriceOfAddedVegetables = 980;
+public class GreenCartTest extends GreenKartBaseTest {
+    private static final int expectedPriceOfAddedVegetables = 980;
 
     public GreenCartTest() {
         xpathLocators.put("ALL_VEGETABLE_NAMES", "//div[@class='product']/h4");
@@ -18,6 +19,11 @@ public class GreenCartTest extends BaseTest {
         xpathLocators.put("CART_ICON", "//a[@class='cart-icon']");
         xpathLocators.put("PROCEED_TO_CHECKOUT_BUTTON", "//div[@class='cart-preview active']/div[@class='action-block']/button");
         xpathLocators.put("CHECKOUT_TOTAL_AMOUNT", "//div[@class='promoWrapper']/following-sibling::span[1]");
+        xpathLocators.put("PLACE_ORDER_BUTTON", "//div[@class='promoWrapper']/following-sibling::button");
+        xpathLocators.put("COUNTRY_DROPDOWN", "//div[@class='products']//select");
+        xpathLocators.put("TERMS_AND_CONDITIONS_CHECKBOX", "//input[@class='chkAgree']");
+        xpathLocators.put("COUNTRY_PROCEED_BUTTON", "//input[@class='chkAgree']/following-sibling::button");
+        xpathLocators.put("FINAL_SUCCESS_MESSAGE", "//div[@id='root']//div[@class='products']/div");
     }
 
     private final List<String> requiredVegetables = List.of("Cauliflower", "Cucumber", "Beetroot", "Carrot");
@@ -73,14 +79,32 @@ public class GreenCartTest extends BaseTest {
     }
 
     @Test(priority = 5)
-    public void proceedToCheckout() throws InterruptedException {
+    public void proceedToCheckout() {
         driver.findElement(By.xpath(xpathLocators.get("CART_ICON"))).click();
         driver.findElement(By.xpath(xpathLocators.get("PROCEED_TO_CHECKOUT_BUTTON"))).click();
-        Thread.sleep(5000);
 
+        waitForElementToBeVisible(xpathLocators.get("CHECKOUT_TOTAL_AMOUNT"));
         int totalPriceOfAddedVegetables = Integer.parseInt(driver.findElement(By.xpath(xpathLocators.get("CHECKOUT_TOTAL_AMOUNT"))).getText());
 
         softAssertions.assertThat(totalPriceOfAddedVegetables).isEqualTo(expectedPriceOfAddedVegetables);
         logger.info("Total price at checkout = Rs. " + totalPriceOfAddedVegetables);
+    }
+
+    @Test(priority = 6)
+    public void placeOrder() {
+        driver.findElement(By.xpath(xpathLocators.get("PLACE_ORDER_BUTTON"))).click();
+
+        Select countryDropdown = new Select(driver.findElement(By.xpath(xpathLocators.get("COUNTRY_DROPDOWN"))));
+        countryDropdown.selectByValue("India");
+
+        driver.findElement(By.xpath(xpathLocators.get("TERMS_AND_CONDITIONS_CHECKBOX"))).click();
+
+        driver.findElement(By.xpath(xpathLocators.get("COUNTRY_PROCEED_BUTTON"))).click();
+
+        String expectedMessage = "Thank you, your order has been placed successfully" + "\n" + "You'll be redirected to Home page shortly!!";
+        String actualMessage = driver.findElement(By.xpath(xpathLocators.get("FINAL_SUCCESS_MESSAGE"))).getText();
+
+        softAssertions.assertThat(actualMessage).isEqualTo(expectedMessage);
+        logger.info(actualMessage.replace("\n", " "));
     }
 }
--- src/test/java/learning/uitesting/selenium/exampleproblems/greenkartvegetables/GreenKartBaseTest.java
+++ src/test/java/learning/uitesting/selenium/exampleproblems/greenkartvegetables/GreenKartBaseTest.java
@@ -3,14 +3,22 @@ package learning.uitesting.selenium.exampleproblems.greenkartvegetables;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.time.Duration;
 import java.util.HashMap;
 
-public class BaseTest {
+public class GreenKartBaseTest {
+    private WebDriverWait webDriverWait;
+
+    private static final long DEFAULT_TIME_OUT_IN_SECONDS = 5;
+
     protected static WebDriver driver;
     protected SoftAssertions softAssertions;
     protected Logger logger;
@@ -31,5 +39,10 @@ public class BaseTest {
     public void tearDown() {
         softAssertions.assertAll();
         driver.quit();
+    }
+
+    public void waitForElementToBeVisible(String xpathLocator) {
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIME_OUT_IN_SECONDS));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathLocator)));
     }
 }
